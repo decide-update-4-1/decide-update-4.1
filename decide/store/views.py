72bdf898a79c13c6a54da54edad9279b30f59326
Alberto Toledo <albtolmay@alum.us.es>
2022-11-20 13:31:43 +0100

    research: Añadida comprobación en views.py de store #18
    Esta comprobación hace que el test de test_gen_vote_invalid sea correcto. Bug: Se ha comprobado que en el booth tiene bug que siempre accede a la votación más antigua creada.

--- decide/store/views.py
+++ decide/store/views.py
@@ -32,12 +32,17 @@ class StoreView(generics.ListAPIView):
         vid = request.data.get('voting')
         voting = mods.get('voting', params={'id': vid})
         if not voting or not isinstance(voting, list):
+            # print("por aqui 35")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
         start_date = voting[0].get('start_date', None)
+        # print ("Start date: "+  start_date)
         end_date = voting[0].get('end_date', None)
+        #print ("End date: ", end_date)
         not_started = not start_date or timezone.now() < parse_datetime(start_date)
+        #print (not_started)
         is_closed = end_date and parse_datetime(end_date) < timezone.now()
         if not_started or is_closed:
+            #print("por aqui 42")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
         uid = request.data.get('voter')
@@ -47,15 +52,20 @@ class StoreView(generics.ListAPIView):
             return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
         # validating voter
-        token = request.auth.key
+        if request.auth:
+            token = request.auth.key
+        else:
+            token = "NO-AUTH-VOTE"
         voter = mods.post('authentication', entry_point='/getuser/', json={'token': token})
         voter_id = voter.get('id', None)
         if not voter_id or voter_id != uid:
+            # print("por aqui 59")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
         # the user is in the census
         perms = mods.get('census/{}'.format(vid), params={'voter_id': uid}, response=True)
         if perms.status_code == 401:
+            # print("por aqui 65")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
         a = vote.get("a")
