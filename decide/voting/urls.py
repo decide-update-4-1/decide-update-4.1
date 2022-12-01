eb106618771f6035e0ea2d0cd921454c5cd81840
Alberto Toledo <albtolmay@alum.us.es>
2022-12-01 20:38:09 +0100

    fix: Arreglado filtrado de votaciones por id para modelo voting #29
    El bug se introduce tras la actualización a django 4.1, tal que el filtrado por votaciones por id deja de ser funcional. En este fix se corrige dicho bug.

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.VotingView.as_view(), name='voting'),
-    path('<int:voting_id>', views.VotingView.as_view(), name='voting'),
     path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
 ]
